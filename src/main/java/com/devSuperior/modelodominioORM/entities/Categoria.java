24dac22fa442e9da9bfe6f4ab0cfaffacc0d41c5
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-11-23 17:39:58 -0300

    [DEV] Cria tabela e relacionamentos de Atividade e Categoria

---
+++ src/main/java/com/devSuperior/modelodominioORM/entities/Categoria.java
@@ -0,0 +1,40 @@
+package com.devSuperior.modelodominioORM.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividades = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
+    }
+}
