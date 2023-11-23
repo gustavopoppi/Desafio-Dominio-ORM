7ca7058b110fc24b69d3ad9cfa556211a50566c0
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-11-23 17:21:45 -0300

    [DEV] Inicia projeto desafio 2 DevSuperior

---
+++ src/test/java/com/devSuperior/modelodominioORM/ModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperior.modelodominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
